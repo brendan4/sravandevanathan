14cdabaeff1514a9f780b6db3cb72f4128e56b62
brendan4 <brendan.panici@utexas.edu>
2019-03-02 13:16:15 -0600

    (R Function version)

--- dataimport.R
+++ dataimport.R
@@ -1,32 +1,45 @@
 install.packages("tidyr")
 library (data.table)
 library(tidyr)
-setwd("C:/Users/brendan/Documents/sravandevanathan/ballgown")
 
-importing <-c("gene_abundance.tab")
+mainTable <- mergeTables(wd = "C:/Users/brendan/Documents/sravandevanathan/ballgown",commonName = "gene_abundance.tab", firstFile= "L2_ACAGTG")
+  # call to function 
 
-files <- list.files(path =".")
-
-
-for(file in files){
-  setwd(paste("C:/Users/brendan/Documents/sravandevanathan/ballgown/",file, sep = ""))
-  print(file)
-  x <- fread(importing, drop = c(1,3,4,7,9)) 
-  x <- unite(x,`Gene Names`,c(`Gene Name`,Start, End), remove = TRUE)
-  names(x)[names(x) == 'FPKM'] <- paste('FPKM', file)
+mergeTables <- function(wd, commonName, firstFile){
+  #function definations: wd = working directory, commonName = share name of files, FirstFile = first file to be imported
   
-  if(file == "L2_ACAGTG"){
-    assign("mainTable",x)
-    } else {
-    mainTable<-merge(mainTable, x,by = "Gene Names", all.x = TRUE)
-    #mainTable <- na.omit(mainTable)
-    assign(file, x)
+  setwd(wd)
+  
+  files <- list.files(path =".")
+  
+  for(file in files){
+    setwd(paste(wd,'/',file, sep = ""))
+    print(file)
+    x <- fread(commonName, drop = c(1,3,4,7,9)) 
+    x <- unite(x,`Gene Names`,c(`Gene Name`,Start, End), remove = TRUE)
+    names(x)[names(x) == 'FPKM'] <- paste('FPKM', file)
+    
+    xnoDups <- x[-c(which(duplicated(x$`Gene Names`)))] # removes duplicates 
+    
+    if(file == firstFile){
+      assign("mainTable",xnoDups)
+        
+      }else {
+      mainTable<-merge(mainTable, xnoDups,by = "Gene Names", all.x = TRUE)
+      #mainTable <- na.omit(mainTable)
+      assign(file, xnoDups)
+    }
   }
+  return(mainTable)
 }
 
 
-merge_data <- function(mainTable, merger){
-  merge(mainTable, merger, all.x = TRUE)
-} 
-merge_data(mainTable,L2_ACTTTGA)
-mainTable<-merge(mainTable, L2_ACTTTGA, by.x = "Gene Names", by.y = "Gene Names",all.x = TRUE, suffixes = files[1])
+#ignore all below this 
+uniqueItems <- unique(mainTable[1])
+dups <- which(duplicated(mainTable$`Gene Names`))
+duplicates <- mainTable[c(dups)]
+here <-as.data.frame(duplicates)
+
+which(L2_ACTTGA$`Gene Names`== "AKAP17A_1591593_1602520")
+
+
