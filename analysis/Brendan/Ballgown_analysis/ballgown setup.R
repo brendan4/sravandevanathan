e895a9d08a79133e5ef994c82d3c2e18affd1fb6
brendan4 <brendan.panici@utexas.edu>
2019-06-04 16:01:21 -0500

    Ballgown class

---
+++ analysis/Brendan/Ballgown_analysis/ballgown setup.R
@@ -0,0 +1,11 @@
+library(ballgown)
+gene.info <- t_data[c(9,10)]# where gene.info is subseted from 
+
+setwd("C:\\Users\\brendan\\Documents\\sravandevanathan\\ballgown") #folder with all the files
+samples.list <- list.files(path =".") # generate sample list: *** all samples included  
+samples.list <- colnames(expressed.genes) # filtered out troublesome cols 
+
+bg = ballgown(samples = samples.list, meas='FPKM') # generation of a ballgown object 
+sampleNames(bg) # sanity check sample names 
+
+
