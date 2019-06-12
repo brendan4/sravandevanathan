508662f5c033a3ca12168cd96192af3ed0b59ccb
brendan4 <brendan.panici@utexas.edu>
2019-06-12 10:23:26 -0500

    Organization

---
+++ analysis/Brendan/Tools/clean.r
@@ -0,0 +1,17 @@
+clean.environment <- function( keep = NULL ){
+  if ( length(keep) == 0 ){
+    keep <- c("expressed.genes", "expressed.trans","discard","clean.environment")
+  }else {
+    keep <- c(keep, "expressed.genes", "expressed.trans","discard","clean.environment")
+  }
+  
+  items <- ls(envir=.GlobalEnv)
+  print(items)
+  discard <- items[-c(which(items %in% keep))]
+  print(discard)
+ 
+  rm(list = c(discard), envir=.GlobalEnv)
+  rm(discard)
+}
+
+clean.environment()
