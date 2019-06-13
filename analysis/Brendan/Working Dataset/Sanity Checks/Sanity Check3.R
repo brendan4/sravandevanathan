25ff754fadbc33197bdece25d085e2a3d4362c1c
brendan4 <brendan.panici@utexas.edu>
2019-06-13 11:13:34 -0500

    Sanity

--- analysis/Brendan/Working Dataset/Sanity Checks/Sanity Check3.R
+++ analysis/Brendan/Working Dataset/Sanity Checks/Sanity Check3.R
@@ -1,12 +1,12 @@
 library(data.table)
-setwd("C:/Users/brendan/Documents/sravandevanathan/analysis/Brendan/merged/Old Ballgown")
+setwd("C:/Users/brendan/Documents/sravandevanathan/analysis/Brendan/01_Merged/Old Ballgown")
 old_data <- fread("gene_abundance_merged.tab") 
 old_data <- as.data.frame(old_data) # changes to data.frame 
 old_data <- old_data[,-which ( colnames(old_data) %in% c("V1"))] # drops V1 col 
 rownames(old_data) <- old_data[,1] # sets names col as rownames 
 old_data <- old_data[,-c(1)] # removes name col
 colnames(old_data) <- substring(colnames(old_data), first = 6) # removes FPKM 
-old_data <- old_data[,-which ( colnames(old_data) %in% c("L2_ACAGTG", "L6_unmatched"))] #removes sample new (and old): optional
+old_data <- old_data[,-which ( colnames(old_data) %in% c("L2_ACAGTG", "L6_unmatched","L2_ACTTTGA"))] #removes sample new (and old): optional
 
 #subsets expressed genes
 new.data <- expressed.genes[,which(colnames(expressed.genes) %in% c(colnames(old_data)))]
@@ -23,6 +23,21 @@ merged.cor.table <- as.data.frame(merged.cor.table)
 #filtering out unneeded cor values 
 old.col <- merged.cor.table[,grep("^old", colnames(merged.cor.table))]
 new.row <- old.col[-grep("^old", rownames(old.col)),]
+
+heatmap(as.matrix(new.row))
+# hierarchical clustering of samples and heatmap of sample similarities
+library(pheatmap)
+pheatmap(new.row, cluster_rows = F, cluster_cols = F) 
+
+library(corrplot)
+library(grDevices)
+corrplot(as.matrix(new.row), cl.lim=c(.85,1), 
+         method = "shade", 
+         is.cor =F,
+         type = "lower",  
+         order = "hclust", 
+         addrect = 2)
+
 
 # remove from workspace
 rm(old_data,old.col,new.row,merged.cor.table,merged.table,new.data)
