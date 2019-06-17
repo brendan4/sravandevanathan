4f525635e585e24f6d43e694976991fb26ae2c95
brendan4 <brendan.panici@utexas.edu>
2019-06-17 09:54:19 -0500

    Project Script

--- analysis/Brendan/Project Script.R
+++ analysis/Brendan/Project Script.R
@@ -1,10 +1,30 @@
 
 library(DBA)
 
-#gene abundance merge
+### Gene abundance merge 
 main.table <- mergeTables(wd = "C:/Users/brendan/Documents/sravandevanathan/ballgown",
                           commonName = "gene_abundance.tab", 
                           colsToMerge = c(2,5,6,8))
 
-GeneAbundance <- merge.cleanup(main.table, cor.table = TRUE)
+main.table <- merge.cleanup(main.table, cor.table = TRUE)
 
+#removal of L2_ACAGTG
+main.table <- main.table[,-which(colnames(main.table) %in% c("FPKM L2_ACAGTG"))]
+
+write.table(main.table, "gene_abundance_merged.tab")
+GeneAbundance <- read.table("gene_abundance_merged.tab")
+
+### Transripts merge
+main.table <- mergeTables(wd = "C:/Users/brendan/Documents/sravandevanathan/ballgown",
+                          commonName = "t_data.ctab", 
+                          colsToMerge = c(4,5,6,10,12))
+
+main.table <- merge.cleanup(main.table, cor.table = TRUE)
+
+#removal of L2_ACAGTG
+main.table <- main.table[,-which(colnames(main.table) %in% c("FPKM L2_ACAGTG"))]
+
+write.table(main.table,gzfile("transrcipts_merged.ctab.gz")) # writes the table as a .ctab.gz
+Transcripts <- read.table(gzfile("transrcipts_merged.ctab.gz")) # will read the table 
+
+rm(main.table)
