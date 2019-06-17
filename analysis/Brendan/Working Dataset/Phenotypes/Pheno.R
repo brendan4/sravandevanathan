11a944031075900f620de98545990a0e369c90bf
brendan4 <brendan.panici@utexas.edu>
2019-06-17 16:09:33 -0500

    Phenotype

--- analysis/Brendan/Working Dataset/Phenotypes/Pheno.R
+++ analysis/Brendan/Working Dataset/Phenotypes/Pheno.R
@@ -2,10 +2,9 @@ library("readxl")
 setwd("C:/Users/brendan/Documents/sravandevanathan/")
 pheno <- read_excel("061518_phenotypes.xlsx")
 
-carriers <- c("L3_CGATGT", "L6_ACAGTC", "L2_ATCACG","L2_GCCAAT","L6_GCCAAT",
-              "L6_CAGATC","L2_TGACCA","L2_ACAGTG","L2_GCCAAT","L2_CAGATC","L2_ACTTGA")
-some.symp <- c("L6_GGCTAC", "L2_GATCAG")
-severe <- c("L6_CGATGT", "L2_CTTGTA")
+carriers <- c(I.one,II.eight,III.two,III.three, III.four)
+some.symp <- III.thirteen
+severe <- IV.five
 
 pheno <- as.data.frame(colnames(expressed.genes))
 pheno$pheno <- 0
@@ -16,7 +15,7 @@ pheno[which(pheno[,2] == 0), 2] <- "W"
 
 # all mutant carriers 
 I.one <- c('L3_ACTTGA', "L6_TTAGGC")
-II.eigth <- c("L3_GGCTAC")
+II.eight <- c("L3_GGCTAC")
 III.two <- c("L3_CGATGT", "L6_ACAGTC", "L2_ATCACG","L2_GCCAAT", "L6_TGACCA")
 III.three <- c("L6_GCCAAT", "L6_ACTTGA","L2_CAGATC")
 III.four <- c("L3_TGACCA", "L6_CAGATC", "L2_CGATGT", "L2_ACTTGA")
@@ -32,9 +31,5 @@ III.six <- c("L3_GCCAAT","L6_GATCAG")
 III.eleven <- c("L3_CAGATC", "L6_TAGCTT")
 III.twelve <- C("L6_CTTGTA", "L2_TAGCTT")
 III.fourteen <- c("L6_ATCACG", "L2_GGCTAC")
-
-
-
-
 
 
