9f418b18c6f98e121505a341cf913eb3f3b635c1
brendan4 <brendan.panici@utexas.edu>
2019-06-17 15:42:50 -0500

    Phenotypes

--- analysis/Brendan/Working Dataset/Phenotypes/Pheno.R
+++ analysis/Brendan/Working Dataset/Phenotypes/Pheno.R
@@ -1,4 +1,8 @@
-carriers <- c("L3_ATCACG", "L3_CGATGT", "L3_TGACCA","L6_ACAGTG","L6_GCCAAT",
+library("readxl")
+setwd("C:/Users/brendan/Documents/sravandevanathan/")
+pheno <- read_excel("061518_phenotypes.xlsx")
+
+carriers <- c("L3_CGATGT", "L6_ACAGTC", "L2_ATCACG","L2_GCCAAT","L6_GCCAAT",
               "L6_CAGATC","L2_TGACCA","L2_ACAGTG","L2_GCCAAT","L2_CAGATC","L2_ACTTGA")
 some.symp <- c("L6_GGCTAC", "L2_GATCAG")
 severe <- c("L6_CGATGT", "L2_CTTGTA")
@@ -10,7 +14,14 @@ pheno[which(pheno[,1] %in% some.symp), 2] <- "S"
 pheno[which(pheno[,1] %in% severe), 2] <- "SS"
 pheno[which(pheno[,2] == 0), 2] <- "W"
 
-
+# all mutant carriers 
+I.one <- c('L3_ACTTGA', "L6_TTAGGC")
+II.eigth <- c("L3_GGCTAC")
+III.two <- c("L3_CGATGT", "L6_ACAGTC", "L2_ATCACG","L2_GCCAAT", "L6_TGACCA")
+III.three <- c("L6_GCCAAT", "L6_ACTTGA","L2_CAGATC")
+III.four <- c("L3_TGACCA", "L6_CAGATC", "L2_CGATGT", "L2_ACTTGA")
+III.thirteen <- c("L6_GGCTAC", "L2_GATCAG")
+IV.five <- c("L2_CTTGTA", "L6_CGATGT")
 
 
 
