008de877e42ba71e292e41a3963beebb9e1e05f8
brendan4 <brendan.panici@utexas.edu>
2019-06-17 16:51:01 -0500

    diff

--- analysis/Brendan/Ballgown Analysis/Prelim Diff.R
+++ analysis/Brendan/Ballgown Analysis/Prelim Diff.R
@@ -1,6 +1,17 @@
+library(dplyr)
+
 pData(bg_filt) = data.frame(id = sampleNames(bg_filt), group = pheno)
 gene.results = stattest(bg_filt, feature="gene", covariate="group.pheno", getFC=TRUE, meas="FPKM")
 
+#gene names 
 indices <- match(gene.results$id, texpr(bg_filt, 'all')$gene_id)
 gene_names_for_result <- texpr(bg_filt, 'all')$gene_name[indices]
 gene.results <- data.frame(geneNames = gene_names_for_result, gene.results)
+
+#processing 
+gene.results = arrange(gene.results, pval)
+filt.names <- subset(gene.results, gene.results$qval<0.05)
+
+#filter
+diff.genes <- filter.genes(na.omit(expressed.genes), filt.names$geneNames)
+test <- filter.genes(expressed.genes, c("RPL", "MDM"))
