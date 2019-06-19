1061db94da01e59531ff44c03252467ff82cd3c7
brendan4 <brendan.panici@utexas.edu>
2019-06-19 15:47:11 -0500

    updates

--- analysis/Brendan/CIBERSORT/data prep.R
+++ analysis/Brendan/CIBERSORT/data prep.R
@@ -24,6 +24,6 @@ expressed.genes.cyber[grepl("^-", expressed.genes.cyber$`1`),1] <-  substring(ex
 
 test <- pretty.gene.name(expressed.genes.cyber, col = 1)
 
-
+#saving test
 write.csv(ExampleMixtures.GEPs,"expressed_genes_cyber.csv",row.names = FALSE, col.names = NA)
 new <-read.csv("expressed_genes_cyber.csv")
