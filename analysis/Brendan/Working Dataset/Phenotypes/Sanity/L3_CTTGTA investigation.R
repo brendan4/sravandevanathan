4045508ce2cf72aaa3196b358133a0d413d42511
brendan4 <brendan.panici@utexas.edu>
2019-06-27 11:56:51 -0500

    Pheno

--- analysis/Brendan/Working Dataset/Phenotypes/Sanity/L3_CTTGTA investigation.R
+++ analysis/Brendan/Working Dataset/Phenotypes/Sanity/L3_CTTGTA investigation.R
@@ -1,3 +1,5 @@
+library(ggpubr)
+
 #sex profile of L3_TTAGGC
 sex.genes <- c("XIST", "USP9Y", "UTY", "RPS4Y1", "TSIX", "PRKY", 'DDX3Y', 'RPS4Y1')
 TTAGGC <- expressed.genes[,grep("TTAGGC", colnames(expressed.genes))]
@@ -12,23 +14,33 @@ sub.filt <- filter.genes(sub, sex.genes)
 cor(na.omit(sub), method = "spearman")
 
 data("full.pheno.table")
-individuals <- full.pheno.table[duplicated(full.pheno.table$Replicates), "Replicates"]
+individuals <- full.pheno.table[!duplicated(full.pheno.table$Replicates), "Replicates"]
 cor.data <- list()
-mean.data <- c()
+mean.data <- data.frame(mean = 0, names = 0)
+sub.counter <- 0 
 
 for(i in 1:length(individuals)){
+  print(individuals[i])
   sub <- full.pheno.table[which(full.pheno.table$Replicates == individuals[i]),]
   sub.data <- expressed.genes[,which(colnames(expressed.genes) %in% sub[,1])]
+  if (length(colnames(sub.data)) == 0){
+    print(paste(individuals[i],": skipped. Only has one sample"))
+    sub.counter <- sub.counter + 1
+  } else{
   cor.table <- cor(na.omit(sub.data), method = "spearman")
-  cor.data[[i]] <- cor.table
-  names(cor.data)[i] <- individuals[i]
-  mean.data[i] <- mean(cor.table)
-  names(mean.data)[i] <- individuals[i]
+  cor.data[[i- sub.counter]] <- cor.table
+  names(cor.data)[i- sub.counter] <- individuals[i- sub.counter]
+  cor.table[which(cor.table == 1)] <- NA
+  mean.data[i- sub.counter,] <- mean(cor.table, na.rm = TRUE)
+  mean.data[i- sub.counter,"names"] <- individuals[i- sub.counter]
+  print(mean.data)
+  }
 }
 
 # least cor between samples
 cor.data[[5]]
 names(cor.data)[5]
 
-
+ggplot(data = mean.data, aes(x=as.factor(names), y = mean, label = names)) + 
+  geom_text()
 
