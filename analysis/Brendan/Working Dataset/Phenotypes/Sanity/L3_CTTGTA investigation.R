e028ba1aa329e9380f43448561fc3c5bf6fcdbb7
brendan4 <brendan.panici@utexas.edu>
2019-06-27 10:08:24 -0500

    phenos

---
+++ analysis/Brendan/Working Dataset/Phenotypes/Sanity/L3_CTTGTA investigation.R
@@ -0,0 +1,22 @@
+#sex profile of L3_TTAGGC
+sex.genes <- c("XIST", "USP9Y", "UTY", "RPS4Y1", "TSIX", "PRKY", 'DDX3Y', 'RPS4Y1')
+TTAGGC <- expressed.genes[,grep("TTAGGC", colnames(expressed.genes))]
+sub.filt <- filter.genes(TTAGGC, sex.genes)
+
+#corelation to other TTAGGC
+cor(na.omit(TTAGGC), method = "spearman")
+
+# corelation to L3_GATCAG individual II.6 (had same barcode in earlier hiseq run)
+sub <- expressed.genes[,which(colnames(expressed.genes) %in% c("L3_TTAGGC", "L3_GATCAG"))]
+sub.filt <- filter.genes(sub, sex.genes)
+cor(na.omit(sub), method = "spearman")
+
+data("full.pheno.table")
+individuals <- full.pheno.table[duplicated(full.pheno.table$Replicates), "Replicates"]
+
+for(i in 1:length(individuals)){
+  sub <- full.pheno.table[which(full.pheno.table$Replicates == individuals[i]),]
+  sub.data <- expressed.genes[,which(colnames(expressed.genes) %in% sub[,1])]
+  cor.table <- cor(na.omit(sub.data))
+}
+  
