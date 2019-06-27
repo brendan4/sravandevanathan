bde8842b50e715378ad2863dffd99e17ffb2563e
brendan4 <brendan.panici@utexas.edu>
2019-06-27 10:44:17 -0500

    Pheno investigation

--- analysis/Brendan/Working Dataset/Phenotypes/Sanity/L3_CTTGTA investigation.R
+++ analysis/Brendan/Working Dataset/Phenotypes/Sanity/L3_CTTGTA investigation.R
@@ -13,10 +13,22 @@ cor(na.omit(sub), method = "spearman")
 
 data("full.pheno.table")
 individuals <- full.pheno.table[duplicated(full.pheno.table$Replicates), "Replicates"]
+cor.data <- list()
+mean.data <- c()
 
 for(i in 1:length(individuals)){
   sub <- full.pheno.table[which(full.pheno.table$Replicates == individuals[i]),]
   sub.data <- expressed.genes[,which(colnames(expressed.genes) %in% sub[,1])]
-  cor.table <- cor(na.omit(sub.data))
+  cor.table <- cor(na.omit(sub.data), method = "spearman")
+  cor.data[[i]] <- cor.table
+  names(cor.data)[i] <- individuals[i]
+  mean.data[i] <- mean(cor.table)
+  names(mean.data)[i] <- individuals[i]
 }
-  
+
+# least cor between samples
+cor.data[[5]]
+names(cor.data)[5]
+
+
+
