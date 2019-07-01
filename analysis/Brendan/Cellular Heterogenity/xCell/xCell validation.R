436197a9bd194fee8b4124f8fdb5b78e7bd433d2
brendan4 <brendan.panici@utexas.edu>
2019-07-01 14:19:29 -0500

    xCell

--- analysis/Brendan/Cellular Heterogenity/xCell/xCell validation.R
+++ analysis/Brendan/Cellular Heterogenity/xCell/xCell validation.R
@@ -4,7 +4,20 @@ setwd("C:/Users/brendan/Documents/sravandevanathan/analysis/Brendan/Cellular Het
 xCell.genesig <- read.xlsx("xCell gene signatures.xlsx")
 
 
+
+test.gene <- rownames(pretty.gene.name(na.omit(expressed.genes), 
+                                       as.row.names = TRUE, 
+                                       remove.dups = TRUE))
+lost <- data.frame(0)
 for(row in 1:nrow(xCell.genesig)){
+  print(paste("Checking celltype:", xCell.genesig[row, 1]))
   
-  
+  for(gene in 1:xCell.genesig[row, 2]){
+    
+    if (xCell.genesig[row, gene +2] %in% test.gene){
+      print(paste(xCell.genesig[row, gene +2],": found in expressed.genes"))
+    }else{
+      print(paste(xCell.genesig[row, gene +2],": NOT found in expressed.genes"))
+    }
+  }
 }
