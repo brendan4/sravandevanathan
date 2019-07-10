a088c0f5c890202746f848dd2a355bed968f6ce5
brendan4 <brendan.panici@utexas.edu>
2019-07-10 10:33:16 -0500

    highly expressed

--- analysis/Brendan/highly expressed.R
+++ analysis/Brendan/highly expressed.R
@@ -10,7 +10,11 @@ GTEX <- GTEX[,which(colnames(GTEX) %in% c("Description","Whole Blood"))]
 means <- apply(expressed.genes, 1, mean)
 means <- na.omit(means)
 
-high.means <- means[which(means > 1000)]
+high.means <- means[which(means > 800)]
+high <- filter.genes(expressed.genes, names(high.means))
+high.GTEX <- GTEX[GTEX$`Whole Blood` > 2000,]
 
+high <- pretty.gene.name(high, as.row.names = TRUE)
 
-
+high.GTEX$Description[which(!high.GTEX$Description %in% high$pretty)]
+high$pretty[which(!high$pretty %in% high.GTEX$Description)]
