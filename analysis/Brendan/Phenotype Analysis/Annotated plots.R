fc54d9bab26420e757466ae7ff3fbc948d82bc22
brendan4 <brendan.panici@utexas.edu>
2019-07-10 09:17:43 -0500

    PCA Heatmap log

--- analysis/Brendan/Phenotype Analysis/Annotated plots.R
+++ analysis/Brendan/Phenotype Analysis/Annotated plots.R
@@ -86,12 +86,14 @@ par(mar=c(7,4,4,2)+0.1)
 png(filename='PCA2_heatmap.png', width=1000, height= 800)
 
 
-Heatmap(heat.genes,
+Heatmap(log(heat.genes +0.1),
         column_names_side = "bottom",
         row_names_side = "left",
         row_hclust_side = "left",
         row_names_gp=gpar(cex=0.6),
         row_hclust_width = unit(3, "cm"),
+        clustering_distance_columns = "spearman",
+        clustering_method_columns = "complete",
         clustering_distance_rows ="maximum",
         clustering_method_rows = "complete",
         bottom_annotation = HeatmapAnnotation(type = full.pheno.table[,c(2)],
@@ -99,3 +101,5 @@ Heatmap(heat.genes,
                                               which = "column",
                                               show_legend=TRUE))
 graphics.off()
+
+?Heatmap
