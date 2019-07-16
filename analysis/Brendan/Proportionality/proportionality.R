69fc0862ae008e28628bf869d8eb4851a18a903e
brendan4 <brendan.panici@utexas.edu>
2019-07-16 09:52:18 -0500

    propr

--- analysis/Brendan/Proportionality/proportionality.R
+++ analysis/Brendan/Proportionality/proportionality.R
@@ -1,17 +1,21 @@
 library(propr)
 
+#for clr
 expressed.genes2 <- na.omit(expressed.genes)
 expressed.genes2 <- pretty.gene.name(expressed.genes2, as.row.names = T, remove.dups = T)
 expressed.genes2 <- t(expressed.genes2)
 keep <- apply(expressed.genes2, 2, function(x) sum(x >= 10) >= 10)
 
-# data prep
+# data prep for alr 
 hemo <- c("HBB", "HBG1", "HBG2", "HBA2", "HBA1", "HSPA4", "PPID") 
-hemo <- filter.genes(expressed.genes, c("HBB", "HBG1", "HBG2", "HBA2", "HBA1") )
+hemo <- filter.genes(expressed.genes, hemo )
 hemo <- pretty.gene.name(hemo, as.row.names = TRUE)
 hemo <- t(hemo)
 
 phi <- propr(expressed.genes2, metric = "rho", select = keep)
+updateCutoffs(phi, cutoff =  seq(.05, .95, .3))
+
+phi <- propr(hemo, metric = "rho", ivar = 8)
 updateCutoffs(phi, cutoff =  seq(.05, .95, .3))
 
 #Subset option 1
@@ -32,9 +36,12 @@ smear(phiHBB.HBG1, prompt = TRUE, plotly = TRUE)
 
 #Rho plots 
 slate(phi, k, prompt = TRUE, plotly = FALSE)
-bucket(phi, group, k = 5, prompt = TRUE, plotly = FALSE)
-prism(phi, k = 5, prompt = TRUE, plotly = TRUE)
-bokeh(phi, k, prompt = TRUE, plotly = FALSE)
-cytescape(phi99, col1, col2, prompt = TRUE, d3 = FALSE)
+bucket(phi99, group = pheno$pheno , k = 5, prompt = TRUE, plotly = FALSE)
+prism(phi99, k = 5, prompt = TRUE, plotly = TRUE)
+bokeh(phi, k = 5, prompt = TRUE, plotly = FALSE)
+cytescape(phi99, prompt = TRUE)
 
 
+smear(phi99, prompt = T, plotly = T)
+pca(phi99, group = pheno$pheno)
+snapshot(phi99, plotly = T)
